9c9f7f4adc14079d4e832c5808b8607ed6461853
Thirumaleshp <thirumaleshpinninti@gmail.com>
2025-03-02 16:46:59 +0530

    Updtaed Python file

--- code.py
+++ code.py
@@ -30,6 +30,25 @@ if tc>tca:
                 per = (tca / tc) * 100
                 j+=1
             print("and",i+j,"class more for 80% attendance")
+
+    elif per > 75 and per < 80:
+        if per > 75:
+            i=0
+            j=0
+            while per > 75:
+                tc +=1
+                per = (tca / tc) * 100
+                i += 1
+            print("You can bunk ",i+j-1,"class(es) and maintain 75% attendance")
+        if per < 80:
+            j=0
+            i=0
+        while per < 80:
+            tca += 1
+            tc += 1
+            per = (tca / tc) * 100
+            j+=1
+        print("and attend ",i+j,"class(es) more for 80% attendance")
     
     else:
         if per > 75:
@@ -49,3 +68,4 @@ if tc>tca:
                 print("and bunk ",i+j-1,"class(es) and still maintain 75% attendance")
 else:
     print('Enter valid details')
+ # type: ignore
